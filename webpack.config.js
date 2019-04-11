2614e0309eb7b52973ccdafd476afa1a90051c5e
GrigoryCode <grigorycode@gmail.com>
2019-04-11 12:49:55 +0300

    Split config modules

--- webpack.config.js
+++ webpack.config.js
@@ -1,53 +1,41 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const merge = require('webpack-merge');
+const pug = require('./webpack/pug');
+const devserver = require('./webpack/devserver');
 
 const PATHS = {
     source: path.join(__dirname, 'src'),
     build: path.join(__dirname, 'dist')
 };
 
-const common = {
-    entry: PATHS.source + '/index.js',
-    output: {
-        path: PATHS.dist,
-        filename: 'main.js'
+const common = merge([
+    {
+        entry: PATHS.source + '/index.js',
+        output: {
+            path: PATHS.dist,
+            filename: '[name].js'
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                filename: 'index.html',
+                chucks: ['index'],
+                template: PATHS.source + '/index.pug'
+            })
+        ],
     },
-    plugins: [
-        new HtmlWebpackPlugin({
-            filename: 'index.html',
-            chucks: ['index'],
-            template: PATHS.source + '/index.pug'
-        })
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.pug$/,
-                loader: 'pug-loader',
-                options: {
-                    pretty: true
-                }
-            }
-        ]
-    },
-};
-
-const developmentConfig = {
-    devServer: {
-        stats: 'errors-only',
-        port: 9000
-    }
-};
+    pug()
+]);
 
 module.exports = function(env) {
     if (env === 'production'){
         return common;
     }
     if (env === 'development'){
-        return Object.assign(
+        return merge([
             {},
             common,
-            developmentConfig,
-        )
+            devserver()
+        ])
     }
 };
