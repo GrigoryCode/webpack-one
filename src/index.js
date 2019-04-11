243507b3d5f6189416c50010ebabefe72b1d1023
GrigoryCode <grigorycode@gmail.com>
2019-04-11 10:12:55 +0300

    Static server

--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
 import createMenu from './menu/menu';
-var menu = createMenu(['Home','Portfolio'], 'menu');
+var menu = createMenu(['Home','Portfolio', 'Contacts'], 'menu');
 document.body.appendChild(menu);
